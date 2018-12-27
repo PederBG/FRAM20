9376478a6b27587ac55a8605edb9aebacbac8170
PederBG <pedergrbr@gmail.com>
2018-12-28 00:11:17 +0100

    Change parameters for icedrift quiverplot

--- scripts/downloadmanager.py
+++ scripts/downloadmanager.py
@@ -298,11 +298,11 @@ class DownloadManager(object):
         y = np.linspace(yc, 0, yc)
 
         print('Making quiverplot from dX and dY ice drift estimates')
-        fig = plt.figure(figsize=(11.9*3, 17.7*3), frameon=False) #TODO: fix hardcode
+        fig = plt.figure(figsize=(11.9*5, 17.7*5), frameon=False) #TODO: fix hardcode
         ax = plt.Axes(fig, [0., 0., 1., 1.])
         ax.set_axis_off()
         fig.add_axes(ax)
-        plt.quiver(x, y, dx, dy, scale=1100, width=0.0007)
+        plt.quiver(x, y, dx, dy, scale=3500, width=0.0007)
         fig.savefig(self.TMPDIR + 'quivertmp.png', transparent=False, format='png')
         Image.open(self.TMPDIR + 'quivertmp.png').convert('L').save(self.TMPDIR + 'quivertmp.jpg','JPEG')
 
