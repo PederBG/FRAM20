534186d73c34456bd150c00ad0b7bf2628ee3639
PederBG <pedergrbr@gmail.com>
2019-01-28 14:19:32 +0100

    Add background image animation on landing page

--- fram/static/js/index.js
+++ fram/static/js/index.js
@@ -1,3 +1,22 @@
 $("#index-dropdown").click(function() {
   $("html, body").animate({ scrollTop: window.innerHeight }, 600);
 });
+
+const NUMB_IMAGES = 7;
+const FADE_TIME = 20;
+const background = $('.cover-image');
+
+url = '../../static/img/bg2.jpeg'
+
+background.css('background-image', 'url(' + url + ')');
+
+let i = 0;
+setInterval(function() {
+  background.fadeTo(FADE_TIME, 0.9);
+  setTimeout(function() { background.css('background-image', 'url(' + "../../static/img/bg" + i.toString() + ".jpeg" + ')') }, FADE_TIME);
+  setTimeout(function() { background.fadeTo(FADE_TIME, 1); }, FADE_TIME);
+  i = i + 1;
+  if (i == NUMB_IMAGES) {
+    i =  0;
+  }
+}, 3000);
