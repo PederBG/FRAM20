de667fd3a1f35bf3b68f36e12f42f51c8cf3d868
PederBG <pedergrbr@gmail.com>
2019-01-28 16:11:42 +0100

    Add preloading for landing page images, change favicon and small general improvements

--- fram/static/js/index.js
+++ fram/static/js/index.js
@@ -1,22 +1,38 @@
-$("#index-dropdown").click(function() {
-  $("html, body").animate({ scrollTop: window.innerHeight }, 600);
-});
-
+// Static variables
 const NUMB_IMAGES = 7;
-const FADE_TIME = 20;
+const FADE_TIME = 300;
+const SLIDE_TIME = 5000
 const background = $('.cover-image');
 
-url = '../../static/img/bg2.jpeg'
+// Get background images
+let images = [];
+for (var j = 0; j < NUMB_IMAGES; j++) {
+  images.push("../../static/img/bg" + j.toString() + ".jpeg" );
+}
 
-background.css('background-image', 'url(' + url + ')');
+// Preload all images into cache
+function preload(){
+  images.forEach(function(elem) {
+    im = new Image();
+    im.src = elem;
+  });
+}
+preload();
 
+// Run image slideshow
 let i = 0;
 setInterval(function() {
-  background.fadeTo(FADE_TIME, 0.9);
-  setTimeout(function() { background.css('background-image', 'url(' + "../../static/img/bg" + i.toString() + ".jpeg" + ')') }, FADE_TIME);
-  setTimeout(function() { background.fadeTo(FADE_TIME, 1); }, FADE_TIME);
+  // background.fadeTo(FADE_TIME, 0.9);
+  background.css('background-image', 'url(' + images[i] + ')');
+  // setTimeout(function() { background.fadeTo(FADE_TIME, 1); }, FADE_TIME);
   i = i + 1;
   if (i == NUMB_IMAGES) {
     i =  0;
   }
-}, 3000);
+}, SLIDE_TIME);
+
+
+// Dropdown handler
+$("#index-dropdown").click(function() {
+  $("html, body").animate({ scrollTop: window.innerHeight }, 600);
+});
