cf4438b64e684d8f45cada3cb67627e100d81fba
PederBG <pedergrbr@gmail.com>
2022-11-30 18:12:40 +0100

    Small fix on dockerfiles

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,2 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-docker build -f $DIR/Dockerfile -t pederbg/fram19 .
+docker build -f $DIR/Dockerfile_updated -t pederbg/fram .
--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,2 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-docker run --net=host -it --privileged -v $(realpath $DIR/..):/root/fram19 --name FRAM19 --rm pederbg/fram19:latest
+docker run --net=host -it --privileged -v $(realpath $DIR/..):/root/fram --name FRAM --rm pederbg/fram:latest
